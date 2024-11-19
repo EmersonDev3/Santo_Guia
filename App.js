888a008659f4b7857cc298077a9dd0adee174a5d
EmersonDev3 <lalo.js.dev@gmail.com>
2024-11-19 20:03:11 -0300

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import IntroScreen from './src/screens/IntroScreen'; 
+import IntroScreen from './src/screens/ProfileSetup/IntroScreen'; 
 import LoginScreen from './src/screens/LoginScreen'; 
 
 const Stack = createStackNavigator();
